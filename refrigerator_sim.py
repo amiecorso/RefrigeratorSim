550bb107bdc0f9af85962e7061a1b356569a4771
Amie Corso <acorso@ripple.com>
2020-09-19 08:19:23 -0700

    more argparsage

--- refrigerator_sim.py
+++ refrigerator_sim.py
@@ -1,35 +1,26 @@
 import argparse
 import os
 import pandas as pd
+import subprocess
 import time
 from simulator import Simulator
 
-path_to_moer_data = "MOER_data/MOERS.csv"
-if not os.path.exists(path_to_moer_data):
-    raise FileNotFoundError("{} doesn't seem to exist...".format(path_to_moer_data))
-
-output_dir = "./output_data/"
-if not os.path.exists(output_dir):
-    os.mkdir(output_dir)
-
-# Read initial MOER data
-all_moer_data = pd.read_csv(path_to_moer_data)
-initial_historical_data = all_moer_data[:288]
-sim_moer_data = all_moer_data[288:-1].reset_index(drop=True)
-
-# number of timesteps (data rows) to process (smaller for testing speed)
-num_timesteps = sim_moer_data.shape[0]  # all rows
-#num_timesteps = 300
-
-simulator = Simulator(sim_moer_data, output_dir, num_timesteps)
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--all', action='store_true', help='Run all three models.')
-    parser.add_argument('--no_data', action='store_true', help='Run model using no data.')
-    parser.add_argument('--forecast_only', action='store_true', help='Run model using 1-hr forecast window.')
-    parser.add_argument('--forecast_and_history', action='store_true', help='Run model using 1-hr forecast and historical avgs.')
+    parser.add_argument('--all', action='store_true', default=False, help='Run all three models.')
+    parser.add_argument('--no_data', action='store_true', default=False, help='Run model using no data.')
+    parser.add_argument('--forecast_only', action='store_true', default=False,
+                        help='Run model using 1-hr forecast window.')
+    parser.add_argument('--forecast_and_history', action='store_true', default=False,
+                        help='Run model using 1-hr forecast and historical avgs.')
+    parser.add_argument('--data_path', action='store', default='MOER_data/MOERS.csv', help='Path to dataset.')
+    parser.add_argument('--timesteps', action='store', default='all',
+                        help='The number of timesteps to run for this simulation, defaults to size of dataset.')
+    parser.add_argument('--clean', default=False, action='store_true',
+                        help='Delete the current output data directory before starting simulations.')
     return parser.parse_args()
+
 
 def end_timer(start_time, sim_id):
     end_time = time.time()
@@ -38,10 +29,33 @@ def end_timer(start_time, sim_id):
     seconds = total_seconds % 60
     print("Simulation '{}' duration: {} min {} sec".format(sim_id, minutes, seconds))
 
+
 if __name__ == '__main__':
     args = parse_args()
 
-    # run simulations
+    # Create output dir for artifact collection
+    output_dir = "./output_data/"
+    if args.clean:
+        subprocess.run(["rm", "-rf", output_dir])
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    # Read initial MOER data and cut off first day pre 3-1
+    all_moer_data = pd.read_csv(args.data_path)
+    initial_historical_data = all_moer_data[:288]
+    sim_moer_data = all_moer_data[288:-1].reset_index(drop=True)
+
+    # Number of timesteps (data rows) defaults to full dataset (shrink via command line args for shorter testing cycles)
+    if args.timesteps == 'all':
+        args.timesteps = sim_moer_data.shape[0]
+    else:
+        args.timesteps = int(args.timesteps)
+    simulator = Simulator(sim_moer_data, output_dir, args.timesteps)
+
+    # Run simulations
+    if not args.no_data and not args.forecast_only and not args.forecast_and_history:
+        args.forecast_and_history = True
+
     if args.no_data or args.all:
         start_time = time.time()
         simulator.run_simulation_without_data(show_plot=False)
@@ -52,8 +66,7 @@ if __name__ == '__main__':
         simulator.run_simulation_with_forecast(show_plot=False)
         end_timer(start_time, 'forecast_only')
 
-    if args.forecast_and_history or args.all or len(args) == 0:
+    if args.forecast_and_history or args.all:
         start_time = time.time()
         simulator.run_simulation_with_forecast_and_historicals(show_plot=False)
         end_timer(start_time, 'forecast_and_history')
-
